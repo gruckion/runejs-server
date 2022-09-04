73afdf78797100ad9d2fa7c8de6ccf4fe40ea896
James Monger <jameskmonger@hotmail.co.uk>
2022-09-04 12:11:17 +0100

    feat: chunk rotation code

--- src/plugins/skills/construction/util/rotations.ts
+++ src/plugins/skills/construction/util/rotations.ts
@@ -1,34 +1,33 @@
+import { getTemplateLocalX, getTemplateLocalY } from '@engine/world/map/region';
+
 type ChunkCoordinate = {
-    x: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
-    y: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
+    x: number; // 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
+    y: number; // 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
 }
 
-export function rotateChunkCoordinate({ x, y }: ChunkCoordinate, orientation: number) {
-    if (orientation === 0) {
-        return { x, y };
-    }
-
+export function rotateChunkCoordinate({ x, y }: ChunkCoordinate, orientation: number, sizeX = 1, sizeY = 1) {
+    // for some reason the base function seems to have orientation 1 and 3 swapped
+    // either its wrong there or here, but this way seems correct based on in-game testing and the unit tests
     if (orientation === 1) {
-        return { x: 7 - y, y: x };
+        const calculatedOrientation = 3;
+
+        return {
+            x: getTemplateLocalX(calculatedOrientation, x, y, sizeX, sizeY),
+            y: getTemplateLocalY(calculatedOrientation, x, y, sizeX, sizeY)
+        };
     }
+
+    if (orientation === 3) {
+        const calculatedOrientation = 1;
+
+        return {
+            x: getTemplateLocalX(calculatedOrientation, x, y, sizeX, sizeY),
+            y: getTemplateLocalY(calculatedOrientation, x, y, sizeX, sizeY)
+        };
+    }
+
+    return {
+        x: getTemplateLocalX(orientation, x, y, sizeX, sizeY),
+        y: getTemplateLocalY(orientation, x, y, sizeX, sizeY)
+    };
 }
-
-
-//            0     1     2     3     4     5     6     7
-//
-//
-//     0                       Y1
-//
-//     1           X0                            X1
-//
-//     2
-//
-//     3     Y0                                        Y2
-//
-//     4
-//
-//     5
-//
-//     6          X3                             X2
-//
-//     7                             Y3
